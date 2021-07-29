d666e6e7c65296b9bd541e8241d71c799e691479
walkera9033 <78572362+walkera9033@users.noreply.github.com>
2021-07-29 03:01:12 +0000

    generate questions

--- 05_question_generator_v1.py
+++ 05_question_generator_v1.py
@@ -1,1 +1,10 @@
-print()
+import random
+
+number_1 = random.randint(1, 10)
+number_2 = random.randint(1, 10)
+
+
+answer = eval("{} + {}".format(number_1, number_2))
+
+print("{} + {}".format(number_1, number_2))
+print("answer", answer)
